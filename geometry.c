3900183eccebbd0adad9f98435fac9a5ea763453
Pavlova Victoria <64736854+NickName03@users.noreply.github.com>
2023-02-24 20:15:08 +0700

    Added file reading

---
+++ geometry.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void parser(FILE* file)
+{
+    char* v = (char*)file;
+    printf("You wanna to parse %s", v);
+}
+
+int main(int argc, char* argv[])
+{
+    FILE* file = NULL;
+    if (argc < 2) {
+        file = stdin;
+    } else if (argc == 2) {
+        if ((file = fopen(argv[1], "r")) == NULL) {
+            printf("\e[1;31mError\e[0m: can't open file \e[1;35m\"%s\"\e[0m\n",
+                   argv[1]);
+            exit(EXIT_FAILURE);
+        } else {
+            parser(file);
+            fclose(file);
+        }
+    } else {
+        printf("\e[1;35mUsage\e[0m: %s <filename>\n", argv[0]);
+    }
+}
