9085652ad2268409d28ce35f48b7c86158b3055d
Pavlova Victoria <64736854+NickName03@users.noreply.github.com>
2023-04-02 15:42:39 +0700

    Updated the parser and added new functions

--- geometry.c
+++ geometry.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define size 25
+#define size 50
 
 enum Errors {
     ER_NAME,       // название
@@ -13,6 +13,7 @@ enum Errors {
     ER_NOT_BRACE_LEFT, //фигурная скобка
     ER_NOT_BRACE_RIGHT,
     ER_EXP_COMMA,
+    ER_EXP_SPACE,
 };
 
 int print_error(int pos, int err)
@@ -52,79 +53,135 @@ int print_error(int pos, int err)
         printf("\e[1;31mError\e[0m at column %d: \e[1;31mexpected ','\e[0m\n",
                pos);
         break;
+    case ER_EXP_SPACE:
+        printf("\e[1;31mError\e[0m at column %d: \e[1;31mexpected SPACE "
+               "between "
+               "points\e[0m\n",
+               pos);
+        break;
     }
     return 1;
 }
 
-int parser(FILE* file, int flag)
+int check_circle(char* inputed_string)
+{
+    const char* mask = "circle";
+    int i = 0;
+    while (inputed_string[i] != '(') {
+        if (inputed_string[i] != mask[i]) {
+            if (i <= strlen(mask) - 1) {
+                printf("\e[1;31mError\e[0m at column %d: \e[1;31mexpected "
+                       "%c\e[0m\n",
+                       i + 1,
+                       mask[i]);
+                return 1;
+            } else {
+                if (inputed_string[i] != ' ') {
+                    printf("\e[1;31mError\e[0m at column %d: \e[1;31mexpected "
+                           "%c or "
+                           "space\e[0m\n",
+                           i + 1,
+                           '(');
+                    return 1;
+                }
+            }
+        }
+        i++;
+    }
+    // сейчас inputed_string[i] == '('
+    i++; // теперь следущий символ
+    int flag_for_space = 0;
+    while (inputed_string[i] != ',') {
+        if ((inputed_string[i - 1] != '(' && inputed_string[i] == ' ')
+            && (inputed_string[i + 1] != ',' && inputed_string[i] == ' ')) {
+            flag_for_space++;
+        }
+        if (flag_for_space > 1) {
+            print_error(i, ER_EXP_COMMA);
+            return 2;
+        }
+        i++;
+    }
+    if (flag_for_space == 0) {
+        print_error(i - 4, ER_EXP_SPACE);
+        return 3;
+    }
+    // сейчас inputed_string[i] == ','
+    i++; // теперь следущий символ
+    while (inputed_string[i] != ')') {
+        if (i == strlen(inputed_string) - 1) {
+            print_error(i + 1, ER_NOT_PARENTHESIS_RIGHT);
+            return 5;
+        }
+        i++;
+    }
+    return 0;
+}
+
+/*int check_triangle(char* inputed_string)
+{
+    const char* mask = "triangle((,,,))";
+    return 0;
+}
+
+int check_poligon(char* inputed_string)
+{
+    const char* mask = "polygon((,,{,}))";
+    return 0;
+}*/
+
+void delete_space(char* inputed_string)
+{
+    int pos = 0;
+    while (inputed_string[pos] != '\0') {
+        if ((inputed_string[pos] == ' ' && inputed_string[pos + 1] == ' ')
+            || (inputed_string[pos] == ' ' && inputed_string[pos - 1] == ' ')) {
+            for (int i = pos; i < strlen(inputed_string); i++) {
+                inputed_string[i] = inputed_string[i + 1];
+            }
+            pos--;
+        }
+        pos++;
+    }
+    // удаление пробела в начале
+    if (inputed_string[0] == ' ') {
+        for (int i = 0; i < strlen(inputed_string); i++) {
+            inputed_string[i] = inputed_string[i + 1];
+        }
+    }
+}
+
+void str_to_lower(char* inputed_string)
+{
+    delete_space(inputed_string);
+    for (int i = 0; i < strlen(inputed_string) - 1; i++) {
+        if (islower(inputed_string[i]) == 0)
+            inputed_string[i] = tolower(inputed_string[i]);
+    }
+}
+
+int parser(FILE* file)
 {
     char figure[size];
-    int position = 0;
-    char sym;
-    int er = 0;
-    const char* mask = "circletrianglepolygon0123456789.,(){}";
-    while ((sym = getc(file)) != EOF) {
-        if (sym == 'q') {
-            break;
+    int result = 0;
+    while (fgets(figure, size, file) != NULL && figure[0] != 'q') {
+        str_to_lower(figure);
+        puts(figure);
+        if (figure[0] != 'c' || figure[0] != 'c' || figure[0] != 'c') {
+            print_error(0, ER_NAME);
         }
-        if (sym == '\n') {
-            if (figure[0] == 'c') {
-                if (flag == 2) {
-                    puts(figure);
-                }
-                er = 0;
-                for (int i = 1; i < 14; i++) {
-                    if (i < 6 && figure[i] != mask[i]) {
-                        printf("\e[1;31mError\e[0m at column %d: "
-                               "\e[1;31mexpected %c\e[0m\n",
-                               i + 1,
-                               mask[i]);
-                        er = 1;
-                        break;
-                    }
-                    if (i == 6 && figure[i] != '(') {
-                        er = print_error(i, ER_NOT_PARENTHESIS_LEFT);
-                        break;
-                    }
-                    if (i > 6 && i < 13 && i != 9 && i != 11
-                        && strchr("0123456789", figure[i]) == NULL) {
-                        er = print_error(i, ER_NOT_POINT);
-                        break;
-                    }
-                    if (i == 9 && figure[i] != ',') {
-                        er = print_error(i, ER_EXP_COMMA);
-                        break;
-                    }
-                    if (i == 11 && figure[i] != '.') {
-                        er = print_error(i, ER_NOT_NUMBER);
-                        break;
-                    }
-                    if (i == 14 && figure[i] != ')') {
-                        er = print_error(i, ER_NOT_PARENTHESIS_RIGHT);
-                        break;
-                    }
-                }
-            } else {
-                if (figure[0] == 't') {
-                    puts(figure);
-                } else {
-                    if (figure[0] == 'p') {
-                        puts(figure);
-                    } else {
-                        print_error(0, ER_NAME);
-                    }
-                }
-            }
-            memset(&figure[0], 0, size);
-            position = 0;
-            if (er == 0) {
-                printf("Inputed!\n");
-            }
+        if (figure[0] == 'c') {
+            result = check_circle(figure);
+            if (result == 0)
+                printf("\nInputed!\n");
         }
-        if (strchr(mask, tolower(sym)) != 0) {
-            figure[position] = tolower(sym);
-            position++;
+        if (figure[0] == 't') {
+            check_circle(figure);
         }
+        if (figure[0] == 'p') {
+            check_circle(figure);
+        }
+        memset(&figure[0], 0, strlen(figure));
     }
     return 0;
 }
@@ -133,14 +190,14 @@ int main(int argc, char* argv[])
 {
     FILE* file = NULL;
     if (argc < 2) {
-        parser(stdin, 1);
+        parser(stdin);
     } else if (argc == 2) {
         if ((file = fopen(argv[1], "r")) == NULL) {
             printf("\e[1;31mError\e[0m: can't open file \e[1;35m\"%s\"\e[0m\n",
                    argv[1]);
             return 1;
         } else {
-            parser(file, 2);
+            parser(file);
             fclose(file);
         }
     } else {
